9a58ee85590513387108209a1a6b8e41e97b884c
Hutomo Sugianto <hutomo@premiro.com>
2019-06-26 07:39:24 +0700

    add port

--- public/mysql.php
+++ public/mysql.php
@@ -1,6 +1,6 @@
 <?php
 // we connect to localhost at port 3306
-$link = mysql_connect('mysql', 'root', 'admin');
+$link = mysql_connect('mysql:3306', 'root', 'admin');
 if (!$link) {
     die('Could not connect: ' . mysql_error());
 }
